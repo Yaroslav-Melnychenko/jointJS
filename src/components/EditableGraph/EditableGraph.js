ff984cca415bea5fe0c2eb57e884d16bb80fa7a7
Yaroslav-Melnychenko <yasha271828@gmail.com>
2019-07-25 12:09:35 +0300

    prepare for creating links, create rect and circle

--- src/components/EditableGraph/EditableGraph.js
+++ src/components/EditableGraph/EditableGraph.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import { Button, Spin, Icon } from 'antd';
 import { API_URL } from '../../api/constants';
+import { createRectangle, createEllipse } from '../../utils/createNewShapes';
 import './EditableGraph.css';
 
 const joint = require('jointjs');
@@ -49,14 +50,52 @@ class EditableGraph extends Component {
         const { cells, isLoading } = this.state;
 
         if (cells) {
-            graph.set('graphCustomProperty', true);
             graph.fromJSON({ cells });
+        }
+
+        if (this.paper) {
+            const verticesTool = new joint.linkTools.Vertices();
+            const segmentsTool = new joint.linkTools.Segments();
+            const sourceArrowheadTool = new joint.linkTools.SourceArrowhead();
+            const targetArrowheadTool = new joint.linkTools.TargetArrowhead();
+            const sourceAnchorTool = new joint.linkTools.SourceAnchor();
+            const targetAnchorTool = new joint.linkTools.TargetAnchor();
+            const boundaryTool = new joint.linkTools.Boundary();
+            const removeButton = new joint.linkTools.Remove();
+
+            const toolsView = new joint.dia.ToolsView({
+                tools: [
+                    verticesTool, segmentsTool,
+                    sourceArrowheadTool, targetArrowheadTool,
+                    sourceAnchorTool, targetAnchorTool,
+                    boundaryTool, removeButton
+                ]
+            });
+            this.paper.on('link:mouseenter', function(linkView) {
+                linkView.addTools(toolsView);
+                linkView.showTools();
+                // console.log(linkView)
+            });
+            
+            this.paper.on('link:mouseover', function(linkView) {
+                console.log(linkView)
+                // linkView.addTools(toolsView);
+                // linkView.hideTools();
+            });
         }
 
         return (
             <div className="editable-container">
                 { isLoading ? <Spin indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />} className="loader" /> : null }
                 <div id="editableGraph" ref="editableGraph" className={`loading-graph-${isLoading}`} />
+                <div className="graph-panel">
+                    <li onClick={() => createRectangle(joint, graph, [20, 20], [100, 30], '#4b5fde', '#fff', '', '16px')}>
+                        <div className="rectangle" />
+                    </li>
+                    <li onClick={() => createEllipse(joint, graph, { width: 70, height: 70 }, { text: { text: 'Left' }, circle: { fill: '#2ECC71' } }, [150, 140])}>
+                        <div className="circle" />
+                    </li>
+                </div>
                 <Button 
                     onClick={this.saveGraph} 
                     type="primary"
